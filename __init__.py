897fd370ce4fb1753d31f12697c34ebf861eb5c3
GwenStacey <charles.i.purcell@gmail.com>
2019-09-04 20:19:32 -0600

    Added 2 helper functions

--- __init__.py
+++ __init__.py
@@ -9,3 +9,16 @@ import numpy as np
 ZEROS = np.zeros(5)
 ONES = np.ones(10)
 
+def print_nulls(df):
+    nulls = df.isna().sum()
+    columns = df.columns
+    for i, v in zip(columns, nulls):
+        print(f"Total nulls for {i} is {v}")
+
+#cname here is for column name, took i out of list to avoid
+#confusion with the type
+def list_to_column(df, lst, cname):
+    series = pd.Series(lst)
+    df[cname] = series
+
+  
